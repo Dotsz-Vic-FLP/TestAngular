829898ac14ba37c38a944011103b02f756c7e022
Victor Felipe Jr <23023987@saultcollege.ca>
2024-07-09 01:02:05 -0400

    update files

--- employee-management/src/main.ts
+++ employee-management/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideHttpClient} from "@angular/common/http";
+import {appRoutingProviders, routing} from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    appRoutingProviders, ...routing
+  ]
+}).catch((err) => console.error(err));
